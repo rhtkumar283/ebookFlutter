682d3dc2c739b92a08f1c94d6ef67441c85def22
imawasthi97 <prakhar.awasthi67@gmail.com>
2020-11-23 15:54:31 +0530

    icon and theme update

--- lib/widgets/ourTheme.dart
+++ lib/widgets/ourTheme.dart
@@ -8,4 +8,11 @@ class OurTheme {
   final buttonActive = Color.fromRGBO(255, 255, 255, 1);
   final buttonInactive = Color.fromRGBO(46, 50, 59, 1);
   final double headingSize = 18.00;
+
+  // final primanryColor = Color.fromRGBO(255, 255, 255, 1);
+  // final secondaryColor = Color.fromRGBO(41, 45, 54, 1);
+  // final textColor = Colors.black;
+  // final secTextColor = Colors.grey;
+  // final buttonActive = Color.fromRGBO(250, 120, 74, 1);
+  // final buttonInactive = Color.fromRGBO(28, 36, 48, 1);
 }
